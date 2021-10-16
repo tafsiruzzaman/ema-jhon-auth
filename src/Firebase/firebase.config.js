cf1f733e8dcbd4482baab22a9cf4ebae27255f40
tafsiruzzaman <tafsirjaman100@gmail.com>
2021-10-16 16:31:21 +0600

    firebase added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDRDB38y66_rPtAtVhK2FmZmIE145xg7OA",
+  authDomain: "auth-integration-2522e.firebaseapp.com",
+  projectId: "auth-integration-2522e",
+  storageBucket: "auth-integration-2522e.appspot.com",
+  messagingSenderId: "1027918094700",
+  appId: "1:1027918094700:web:4709af42fb720f0b361349"
+};
+
+export default firebaseConfig;
