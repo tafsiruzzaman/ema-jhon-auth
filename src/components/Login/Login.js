3acfca2bbb7c5f5e596ddc5c741143bd37936bda
tafsiruzzaman <tafsirjaman100@gmail.com>
2021-10-16 17:04:59 +0600

    login & registration form added

---
+++ src/components/Login/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Login.css";
+
+const Login = () => {
+    return (
+        <div className="form">
+            <div>
+                <h2>Login</h2>
+                <form onSubmit="">
+                    <input type="email" name="" id="" placeholder="Your Email"/>
+                    <br />
+                    <input type="password" name="" id="" placeholder="Password"/>
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>New to ema-jhon <Link to="/register">Create Account</Link></p>
+                <div>....................or.....................</div>
+                <button className="btn-regular">Google Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
