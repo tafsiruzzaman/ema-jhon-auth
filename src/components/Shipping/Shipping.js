aaa90e72cbceccf46661e851ec16fbee2af73175
tafsiruzzaman <tafsirjaman100@gmail.com>
2022-01-13 20:23:58 +0600

    pagination added

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
 import useAuth from '../../hooks/useAuth';
+import { clearTheCart, getStoredCart } from '../../utilities/fakedb';
 import './Shipping.css';
 
 const Shipping = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const { user } = useAuth();
+    const history = useHistory();
     const onSubmit = data => {
-        console.log(data)
+        const savedCart = getStoredCart();
+        data.order = savedCart;
+
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.insertedId) {
+                    alert('Order processed Successfully');
+                    clearTheCart();
+                    reset();
+                    history.push('/placeorder')
+                }
+            })
     };
     return (
         <div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,80 +1,97 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
-import {addToDb, getStoredCart} from '../../utilities/fakedb';
+import { addToDb } from '../../utilities/fakedb';
 import './Shop.css';
+import useCart from '../../hooks/useCart';
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useCart();
+    const [page, setPage] = useState(0);
+    const [pageCount, setPageCount] = useState(0);
+    // products to be rendered on the UI
     const [displayProducts, setDisplayProducts] = useState([]);
-    useEffect( () => {
-        fetch('./products.json')
-        .then(res => res.json())
-        .then(data => {
-            setProducts(data);
-            setDisplayProducts(data);
-        })
-    }, []);
-    useEffect( () => {
-        if(products.length) {
-            const savedCart = getStoredCart();
-            const storedCart = [];
-            for (const key in savedCart) {
-            const addedProduct = products.find(product => product.key === key);
-            if (addedProduct){
-                const quantity = savedCart[key];
-                addedProduct.quantity= quantity;
-                storedCart.push(addedProduct);
-            }
-            };
-            setCart(storedCart)
-        }
-    }, [products])
-    
+    const size = 10;
+    useEffect(() => {
+        fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data.products);
+                setDisplayProducts(data.products);
+                const count = data.count;
+                const pageNumber = Math.ceil(count / size);
+                setPageCount(pageNumber);
+            });
+    }, [page]);
+
+
+
     const handleAddToCart = (product) => {
-        const newCart = [...cart];
-        let exist = false;
-        for (const prod of newCart) {
-            if(prod.key === product.key) {
-                prod.quantity = prod.quantity + 1;
-                exist = true;
-            };
+        const exists = cart.find(pd => pd.key === product.key);
+        let newCart = [];
+        if (exists) {
+            const rest = cart.filter(pd => pd.key !== product.key);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, product];
         }
-        if (exist === false) {
-            newCart.push(product)
-        };
-        
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
         setCart(newCart);
+        // save to local storage (for now)
         addToDb(product.key);
+
     }
 
     const handleSearch = event => {
         const searchText = event.target.value;
-        const matchedProduct = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
-        setDisplayProducts(matchedProduct);
+
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+
+        setDisplayProducts(matchedProducts);
     }
+
     return (
-        <div>
-            <div className="input-container">
-                <input className="search-input" type="text" onChange={handleSearch} placeholder="search products" />
+        <>
+            <div className="search-container">
+                <input
+                    type="text"
+                    onChange={handleSearch}
+                    placeholder="Search Product" />
             </div>
             <div className="shop-container">
                 <div className="product-container">
                     {
-                        displayProducts.map(product => <Product key={product.key} product={product} handleAddToCart={handleAddToCart}></Product>)
+                        displayProducts.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        >
+                        </Product>)
                     }
+                    <div className="pagination">
+                        {
+                            [...Array(pageCount).keys()]
+                                .map(number => <button
+                                    className={number === page ? 'selected' : ''}
+                                    key={number}
+                                    onClick={() => setPage(number)}
+                                >{number + 1}</button>)
+                        }
+                    </div>
                 </div>
                 <div className="cart-container">
                     <Cart cart={cart}>
                         <Link to="/review">
-                            <button className="btn-regular">Review your order</button>
+                            <button className="btn-regular">Review Your Order</button>
                         </Link>
                     </Cart>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
